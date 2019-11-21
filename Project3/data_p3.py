944c5696b7ce5871dd8529406293a8897eb8c6b0
Jolynde <jolynde.vis@icloud.com>
2019-11-21 15:26:48 +0100

    histogram plot

--- Project3/data_p3.py
+++ Project3/data_p3.py
@@ -15,6 +15,8 @@ from keras.models import Model, Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor
 
+from sklearn import svm
+
 from classes_p3 import *
 
 dfr = pd.read_csv('./Data/winequality-red.csv', sep = ';')
@@ -37,6 +39,11 @@ def classification_report_with_accuracy_score(y_true, y_pred):
 
 cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
 
+from sklearn.metrics import make_scorer
+def MAD(y_true, y_pred):
+    return (sum(abs(y_true - y_pred)) / len(y_true))
+
+scoring = {'MSE': 'neg_mean_squared_error', 'MAD': make_scorer(MAD, greater_is_better = False), 'R2': 'r2'}
 
 #######
 #BINARY CLASSIFICATION
@@ -118,7 +125,7 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.savefig('./Images/loss_hist.png')
 plt.show()
 """
-
+"""
 #######
 #CATEGORICAL CLASSIFICATION
 #######
@@ -134,8 +141,7 @@ X_ = sc.transform(X)
 
 NNc = NNclassifier()
 
-from sklearn import svm
-svclassifier = svm.SVC(kernel='rbf', gamma = 'auto')
+svclassifier = svm.SVC(kernel='rbf', gamma = 'auto', C = 1.0) #default parameters
 #svclassifier.fit(Xtrain, ytrain)
 #y_pred = svclassifier.predict(Xtest)
 
@@ -143,7 +149,7 @@ svclassifier = svm.SVC(kernel='rbf', gamma = 'auto')
 #print(classification_report(ytest,y_pred))
 
 scores_SVMc = cross_val_score(svclassifier, X_, y, cv = cv)
-print(scores_SVMc)
+#print(scores_SVMc)
 print('CV accuracy score SVMc: %0.3f (+/- %0.3f)' % (scores_SVMc.mean(), scores_SVMc.std()*2))
 
 
@@ -166,7 +172,7 @@ fig, ax = plt.subplots(figsize = (10, 10))
 sns.heatmap(test_acc, annot=True, ax=ax, cmap="viridis", fmt = '.3g')
 ax.set_yticklabels(C_vals)
 ax.set_xticklabels(gamma_vals)
-ax.set_title("MSE test data")
+ax.set_title("Accuracy test data")
 ax.set_xlabel("$\gamma$")
 ax.set_ylabel("C")
 fig.savefig('./Images/heatmap_svmclass1.png')
@@ -174,29 +180,41 @@ plt.show()
 
 ngammas = 100
 gammas = np.logspace(-4, 4, ngammas)
-test_acc = np.zeros(ngammas)
+test_acc_g = np.zeros(ngammas)
 i = 0
 for gam in gammas:
     model = svm.SVC(kernel='rbf', gamma = gam)
     cv_ = cross_val_score(model, X_, y, cv = cv, n_jobs = -1, verbose = 0)
-    test_acc[i] = np.mean(cv_)
+    test_acc_g[i] = np.mean(cv_)
     i += 1
 
-plt.figure()
-plt.plot(np.log10(gammas), test_acc)
-plt.xlabel('log10(gammas)')
-plt.ylabel('accuracy')
-plt.title('SVM classification for different gammas')
-plt.show()
+nc = 100
+cs = np.logspace(-4, 4, ngammas)
+test_acc_c = np.zeros(ngammas)
+i = 0
+for c in cs:
+    model = svm.SVC(kernel='rbf', C = c, gamma = 'auto')
+    cv_ = cross_val_score(model, X_, y, cv = cv, n_jobs = -1, verbose = 0)
+    test_acc_c[i] = np.mean(cv_)
+    i += 1
 
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey = True)
+ax1.plot(np.log10(cs), test_acc_c, 'tab:orange')
+ax1.set_title('C parameter')
+ax1.set_ylabel('Accuracy score')
+ax1.set_xlabel('log10(C)')
+ax2.plot(np.log10(gammas), test_acc_g, 'tab:green')
+ax2.set_title('$\gamma$ parameter')
+ax2.set_xlabel('log10($\gamma$)')
+fig.savefig('./Images/C_gamma_parameters')
+plt.show()
 
 svclassifier = svm.SVC(kernel='rbf', C = 0.1, gamma = 0.1)
 scores_SVMc = cross_val_score(svclassifier, X_, y, cv = cv)
-print(scores_SVMc)
+#print(scores_SVMc)
 print('CV accuracy score tuned SVMc: %0.3f (+/- %0.3f)' % (scores_SVMc.mean(), scores_SVMc.std()*2))
 
-
-"""
+# NEURAL NETWORK MOD
 # Fit the model
 history = NNc.build_network().fit(Xtrain, ytrain, validation_split=0.3, epochs=100, batch_size=32)
 # list all data in history
@@ -226,30 +244,137 @@ scores_NNc = cross_val_score(NNc, X_, y_c, cv = cv)
 print(scores_NNc)
 
 print('CV accuracy score NNc: %0.3f (+/- %0.3f)' % (scores_NNc.mean(), scores_NNc.std()*2))
-
+"""
 
 #######
 # REGRESSION
 #######
 
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2) #, random_state = 0)
 Xtrain = sc.fit_transform(Xtrain)
 Xtest = sc.transform(Xtest)
 X_ = sc.transform(X)
 
-NNr_ = NNregressor(len_X = Xtrain)
+#LINEAR REGRESSION
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.metrics import mean_squared_error
+
+score = []
+reg = Ridge(alpha = 0.7)
+
+for i in range(1000):
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2) #, random_state = 0)
+    reg.fit(Xtrain, ytrain)
+    ypred = reg.predict(Xtest)
+
+    score.append(mean_squared_error(ytest, ypred))
+
+sns.distplot(score, label = 'ridge')
+
+score = []
+reg1 = svm.SVR(kernel = 'rbf', gamma = 'auto')
+
+for i in range(1000):
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2) #, random_state = 0)
+    reg1.fit(Xtrain, ytrain)
+    ypred = reg1.predict(Xtest)
+
+    score.append(mean_squared_error(ytest, ypred))
+
+sns.distplot(score, label = 'svm')
+plt.legend()
+plt.show()
+
+
+"""
+cv_reg = cross_validate(reg, X_, y, cv = cv, n_jobs = -1, scoring = scoring, return_train_score = False)
+
+print('MSE OLS: %0.5f (+/- %0.5f)' % (-cv_reg['test_MSE'].mean(), cv_reg['test_MSE'].std()*2))
+print('R2 OLS: %0.5f (+/- %0.5f)' % (cv_reg['test_R2'].mean(), cv_reg['test_R2'].std()*2))
+print('MAD OLS: %0.5f (+/- %0.5f)' % (-cv_reg['test_MAD'].mean(), cv_reg['test_MAD'].std()*2))
+print('---')
+
+
+#SVM REGRESSOR
+SVMr = svm.SVR(kernel = 'rbf', gamma = 'auto')
+
+cv_SVMr = cross_validate(SVMr, X_, y, cv = cv, n_jobs = -1, scoring = scoring, return_train_score = False)
+#print('cv_scores_nn_test:', cv_SVMr['test_MSE'])
+print('MSE SVM: %0.5f (+/- %0.5f)' % (-cv_SVMr['test_MSE'].mean(), cv_SVMr['test_MSE'].std()*2))
+print('R2 SVM: %0.5f (+/- %0.5f)' % (cv_SVMr['test_R2'].mean(), cv_SVMr['test_R2'].std()*2))
+print('MAD SVM: %0.5f (+/- %0.5f)' % (-cv_SVMr['test_MAD'].mean(), cv_SVMr['test_MAD'].std()*2))
+print('---')
+
+
+C_vals = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e2, 1e3, 1e4] #[0.01, 0.1, 1, 10, 1e2, 1e4, 1e6]
+gamma_vals = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e2, 1e3, 1e4] #[10, 1, 0.1, 0.01, 0.0001, 1e-5, 1e-7]
+scores = np.zeros((len(C_vals), len(gamma_vals)))
+test_mse = np.zeros((len(C_vals), len(gamma_vals)))
+train_mse = np.zeros((len(C_vals), len(gamma_vals)))
+for i, gamma in enumerate(gamma_vals):
+    for j, C in enumerate(C_vals):
+        model = svm.SVR(kernel = 'rbf', C = C, gamma = gamma)
+        cv_ = cross_validate(model, X_, y, cv = cv, scoring = 'neg_mean_squared_error', n_jobs = -1, verbose = 0, return_train_score = True)
+        print(cv_.keys())
+        scores = np.mean(-cv_['test_score'])
+        test_mse[i][j] = np.mean(-cv_['test_score'])
+        train_mse[i][j] = np.mean(-cv_['train_score'])
+        print('C = ', C)
+        print('gamma = ', gamma)
+        print('MSE:', scores)
+
+# plot heatmap of MSE from grid search over eta and lambda
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(train_mse, annot=True, ax=ax, cmap="viridis", fmt = '.3g')
+ax.set_yticklabels(gamma_vals)
+ax.set_xticklabels(C_vals)
+ax.set_title("MSE train data")
+ax.set_ylabel("$\gamma$")
+ax.set_xlabel("C")
+fig.savefig('./Images/heatmap_svmreg_train.png')
+plt.show()
+
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(test_mse, annot=True, ax=ax, cmap="viridis", fmt = '.3g')
+ax.set_xticklabels(C_vals)
+ax.set_yticklabels(gamma_vals)
+ax.set_title("MSE test data")
+ax.set_ylabel("$\gamma$")
+ax.set_xlabel("C")
+fig.savefig('./Images/heatmap_svmreg.png')
+plt.show()
+
+
+SVMr_tuned = svm.SVR(kernel = 'rbf', gamma = 0.1, C = 1.0)
+cv_SVMr_tuned = cross_validate(SVMr_tuned, X_, y, cv = cv, n_jobs = -1, scoring = scoring, return_train_score = False)
+#print('cv_scores_nn_test:', cv_SVMr_tuned['test_MSE'])
+print('MSE SVM_tuned: %0.5f (+/- %0.5f)' % (-cv_SVMr_tuned['test_MSE'].mean(), cv_SVMr_tuned['test_MSE'].std()*2))
+print('R2 SVM_tuned: %0.5f (+/- %0.5f)' % (cv_SVMr_tuned['test_R2'].mean(), cv_SVMr_tuned['test_R2'].std()*2))
+print('MAD SVM_tuned: %0.5f (+/- %0.5f)' % (-cv_SVMr_tuned['test_MAD'].mean(), cv_SVMr_tuned['test_MAD'].std()*2))
+print('---')
+
+# NEURAL NETWORK REGRESSOR
+layer_size = (100,20)
+epochs = 50
+batch_size = 16
+lambda_ = 0
+activation_function = 'relu'
+
+NNr_ = NNregressor(layer_sizes = layer_size, activation_function = activation_function, alpha = lambda_, len_X = Xtrain)
 
 # Fit the model
 #history = NNr.build_network().fit(Xtrain, ytrain, validation_split=0.3, epochs=100, batch_size=32)
 
-NNr = KerasRegressor(build_fn=NNr_.build_network, verbose = 1)
-scores_NNr = cross_validate(NNr, X, y, cv = cv, scoring = ('neg_mean_squared_error', 'r2'), return_train_score = False, verbose = 1, n_jobs = -1)
-print('cv_scores_keras:', scores_NNr['test_neg_mean_squared_error'])
-print('MSE keras test: %0.5f (+/- %0.5f)' % (-np.mean(scores_NNr['test_neg_mean_squared_error']), np.std(scores_NNr['test_neg_mean_squared_error'])*2))
-print('R2 keras test: %0.5f (+/- %0.5f)' % (np.mean(scores_NNr['test_r2']), np.std(scores_NNr['test_r2'])*2))
+NNr = KerasRegressor(build_fn=NNr_.build_network, epochs = epochs, batch_size = batch_size, verbose = 0)
+scores_NNr = cross_validate(NNr, X, y, cv = cv, scoring = scoring, return_train_score = False, verbose = 0, n_jobs = -1)
+print('cv_scores_keras:', scores_NNr['test_MSE'])
+print('MSE keras test: %0.5f (+/- %0.5f)' % (-np.mean(scores_NNr['test_MSE']), np.std(scores_NNr['test_MSE'])*2))
+print('R2 keras test: %0.5f (+/- %0.5f)' % (np.mean(scores_NNr['test_R2']), np.std(scores_NNr['test_R2'])*2))
+print('MAD keras test: %0.5f (+/- %0.5f)' % (-np.mean(scores_NNr['test_MAD']), np.std(scores_NNr['test_MAD'])*2))
+print('---')
 
 # Fit the model
-history = NNr_.build_network().fit(Xtrain, ytrain, validation_split=0.3, epochs=100, batch_size=32)
+history = NNr_.build_network().fit(Xtrain, ytrain, validation_split=0.3, epochs=100, batch_size=batch_size)
 # list all data in history
 print(history.history.keys())
 # summarize history for accuracy
@@ -273,12 +398,9 @@ plt.show()
 
 # Best regularization parameter
 lmbd_vals = [0, 0.00001, 0.0001, 0.01, 0.1]
-
 nlambdas = 10
 lambdas = np.logspace(-7, 2, nlambdas)
-
 test_MSE = np.zeros(nlambdas)
-
 i = 0
 for lmbd in lambdas:
     DNN = NNregressor(alpha=lmbd, len_X = Xtrain)
@@ -293,4 +415,31 @@ plt.xlabel('log10(lambda)')
 plt.ylabel('MSE')
 plt.title('NN regression for different lambdas')
 plt.show()
+
+#grid search
+parameters = {'layer_sizes':([128],[128,64],[256,128,64,32]), 'activation_function':['sigmoid', 'relu'], 'alpha':[0, 0.01, 0.03, 0.1], 'batch_size': [16,32,64]}
+nnRegressor = KerasRegressor(build_fn=NNr_.build_network, verbose=0)
+gs_nn = GridSearchCV(nnRegressor, parameters, scoring = 'mse', cv=cv, verbose = 0, n_jobs=-1)
+gs_nn.fit(Xtrain, ytrain)
+best_parameters = gs_nn.best_params_
+print(best_parameters)
+
+#NOT FINISHED
+
+#optimized model
+layer_size = (100,20)
+epochs = 50
+batch_size = 16
+lambda_ = 0
+activation_function = 'relu'
+
+NNr_ = NNregressor(layer_sizes = layer_size, activation_function = activation_function, alpha = lambda_, len_X = Xtrain)
+
+NNr = KerasRegressor(build_fn=NNr_.build_network, epochs = epochs, batch_size = batch_size, verbose = 0)
+scores_NNr = cross_validate(NNr, X, y, cv = cv, scoring = scoring, return_train_score = False, verbose = 0, n_jobs = -1)
+print('cv_scores_keras:', scores_NNr['test_MSE'])
+print('MSE keras optimized: %0.5f (+/- %0.5f)' % (-np.mean(scores_NNr['test_MSE']), np.std(scores_NNr['test_MSE'])*2))
+print('R2 keras optimized: %0.5f (+/- %0.5f)' % (np.mean(scores_NNr['test_R2']), np.std(scores_NNr['test_R2'])*2))
+print('MAD keras optimized: %0.5f (+/- %0.5f)' % (-np.mean(scores_NNr['test_MAD']), np.std(scores_NNr['test_MAD'])*2))
+print('---')
 """
